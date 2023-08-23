608917de8b33ebbcc5afbdf1125840e2eb88a7b8
Bryan Fischer <myemfar@gmail.com>
2023-08-23 16:36:27 -0600

    put drinks and errors

--- api/queries/drinks.py
+++ api/queries/drinks.py
@@ -32,10 +32,11 @@ class DrinksRepo:
 
     def get(self, drink_id):
         test_drink = self.get_all()
-
+        exists = False
         for item in test_drink:
             if int(drink_id) == int(item["id"]):
-                break
+                exists = True
+        if exists == False:
             raise DrinkNotFound
 
         with pool.connection() as conn:
@@ -63,11 +64,42 @@ class DrinksRepo:
 
                 return result
 
+    # def _get_by_name(self, drink_name):
+    #     with pool.connection() as conn:
+    #         with conn.cursor() as db:
+    #             db.execute(
+    #                 """
+    #                 SELECT *
+    #                 FROM drinks
+    #                 WHERE name = %s;
+    #                 """,
+    #                 [drink_name],
+    #             )
+    #             result = []
+    #             row = db.fetchone()
+
+    #             record = {
+    #                 "id": row[0],
+    #                 "name": row[1],
+    #                 "image_url": row[2],
+    #                 "description": row[3],
+    #                 "instructions": row[4],
+    #             }
+
+    #             result.append(record)
+
+    #             return result
+
     def add_drink(self, info: DrinksIn):
+        # if self._get_by_name(info.name):
+        #     raise DrinkAlreadyExists
         test_drink = self.get_all()
+        exists = False
         for item in test_drink:
             if info.name == item["name"]:
-                raise DrinkAlreadyExists
+                exists = True
+        if exists == True:
+            raise DrinkAlreadyExists
         with pool.connection() as conn:
             with conn.cursor() as db:
                 db.execute(
@@ -83,6 +115,34 @@ class DrinksRepo:
                         info.image_url,
                         info.description,
                         info.instructions,
+                    ],
+                )
+                record = None
+                row = db.fetchone()
+                if row is not None:
+                    record = {}
+                    for i, column in enumerate(db.description):
+                        record[column.name] = row[i]
+
+                return record
+
+    def update_drink(self, info: DrinksIn):
+        with pool.connection() as conn:
+            with conn.cursor() as db:
+                db.execute(
+                    """
+                    UPDATE drinks
+                    SET image_url = %s,
+                    description = %s,
+                        instructions = %s
+                    WHERE name = %s
+                    RETURNING id, name, image_url, description, instructions;
+                    """,
+                    [
+                        info.image_url,
+                        info.description,
+                        info.instructions,
+                        info.name,
                     ],
                 )
                 record = None
--- api/routers/drinks.py
+++ api/routers/drinks.py
@@ -7,7 +7,7 @@ from fastapi import (
     status,
 )
 from models import DrinksIn, DrinksOut, DrinksList
-from queries.drinks import DrinksRepo, DrinkNotFound
+from queries.drinks import DrinksRepo, DrinkNotFound, DrinkAlreadyExists
 
 
 router = APIRouter()
@@ -40,5 +40,20 @@ def add_drink(
     info: DrinksIn,
     repo: DrinksRepo = Depends(),
 ):
-    added_drink = repo.add_drink(info)
+    try:
+        added_drink = repo.add_drink(info)
+    except DrinkAlreadyExists:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Drink already exists",
+        )
     return added_drink
+
+
+@router.put("/api/drinks/", response_model=DrinksOut)
+def update_drink(
+    info: DrinksIn,
+    repo: DrinksRepo = Depends(),
+):
+    updated_drink = repo.update_drink(info)
+    return updated_drink
