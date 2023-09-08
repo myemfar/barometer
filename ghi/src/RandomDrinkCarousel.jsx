9cdb05ad286e9745ce3bfb04f0778f99c3c7e3c2
Bryan Fischer <myemfar@gmail.com>
2023-09-08 14:00:41 -0600

    dummy instructor data in carosel

--- ghi/src/RandomDrinkCarousel.jsx
+++ ghi/src/RandomDrinkCarousel.jsx
@@ -2,28 +2,26 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import { useGetDrinkQuery } from "./app/apiSlice";
 import { NavLink } from "react-router-dom";
+import dummyData from "./dummyData";
 
 function RandomDrinkCarousel() {
   const { data: drinks, isLoading: drinksLoading } = useGetDrinkQuery();
 
   if (drinksLoading) return <div>Loading..</div>;
 
-  // Function to shuffle an array randomly
   function shuffleArray(array) {
     const shuffled = [...array];
     for (let i = shuffled.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap elements
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
     }
     return shuffled;
   }
 
-  // Shuffle the drinks array randomly
   const shuffledDrinks = shuffleArray(drinks);
 
-  // Select the first 5 drinks from the shuffled array
-  const random5Drinks = shuffledDrinks.slice(0, 5);
-
+  const random5Drinks = drinks.length > 0 ? drinks : dummyData.slice(0, 5);
+  console.log(random5Drinks);
   return (
     <div className="carousel-wrapper-123custom">
       <Carousel>
@@ -32,9 +30,17 @@ function RandomDrinkCarousel() {
             <img src={drink.image_url} alt={drink.name} />
             <Carousel.Caption>
               <p>{drink.description}</p>
-              <NavLink className="btn btn-secondary" to={`/drinks/${drink.id}`}>
-                {drink.name}
-              </NavLink>
+              {drinks.length > 0 && (
+                <NavLink
+                  className="btn btn-secondary"
+                  to={`/drinks/${drink.id}`}
+                >
+                  {drink.name}
+                </NavLink>
+              )}
+              {drinks.length == 0 && (
+                <button className="btn btn-secondary">{drink.name}</button>
+              )}
             </Carousel.Caption>
           </Carousel.Item>
         ))}
---
+++ ghi/src/dummyData.js
@@ -0,0 +1,37 @@
+const dummyData = [
+  {
+    instructions:
+      "This data should be unneeded for carosel, DUMMY INSTRUCTIONS",
+    user_id: 0,
+    id: 1,
+    name: "Riley's Pika Cola",
+    image_url:
+      "https://www.myamericanmarket.com/media/catalog/product/cache/6d4a09ff1db2ecc28ee9c6b265bf55a1/q/d/qdol-pokemon-citrus-fruits-sparkling-water-1.jpg",
+    description: "Sweet and Sour, drinking this makes you feel helpful",
+  },
+  {
+    instructions:
+      "This data should be unneeded for carosel, DUMMY INSTRUCTIONS",
+    user_id: 0,
+    id: 2,
+    name: "Josh's Covfefe",
+    image_url:
+      "https://media.cnn.com/api/v1/images/stellar/prod/150929101049-black-coffee-stock.jpg",
+    description:
+      "Drink too many, drink all day, doesn't matter because it's delicious",
+  },
+  {
+    instructions:
+      "This data should be unneeded for carosel, DUMMY INSTRUCTIONS",
+    user_id: 0,
+    id: 3,
+    name: "Michael's Water",
+    image_url:
+      "http://content.health.harvard.edu/wp-content/uploads/2023/07/b8a1309a-ba53-48c7-bca3-9c36aab2338a.jpg",
+    description:
+      "We don't actually know what Michael drinks, but everyone drinks water!",
+  },
+  // Add more dummy drink objects as needed
+];
+
+export default dummyData;
