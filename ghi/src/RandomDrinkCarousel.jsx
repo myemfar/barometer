4d3571f8aea057cbeb74d3353a47c40baac086b9
Bryan Fischer <myemfar@gmail.com>
2023-09-08 14:22:00 -0600

    carosel looks gud

--- ghi/src/RandomDrinkCarousel.jsx
+++ ghi/src/RandomDrinkCarousel.jsx
@@ -8,7 +8,7 @@ function RandomDrinkCarousel() {
   const { data: drinks, isLoading: drinksLoading } = useGetDrinkQuery();
 
   if (drinksLoading) return <div>Loading..</div>;
-
+  const drinkSubset = drinks.slice(0, 50);
   function shuffleArray(array) {
     const shuffled = [...array];
     for (let i = shuffled.length - 1; i > 0; i--) {
@@ -18,10 +18,12 @@ function RandomDrinkCarousel() {
     return shuffled;
   }
 
-  const shuffledDrinks = shuffleArray(drinks);
+  const shuffledDrinks = shuffleArray(drinkSubset);
 
-  const random5Drinks = drinks.length > 0 ? drinks : dummyData.slice(0, 5);
-  console.log(random5Drinks);
+  const random5Drinks =
+    shuffledDrinks.length > 0
+      ? shuffledDrinks.slice(0, 5)
+      : dummyData.slice(0, 5);
   return (
     <div className="carousel-wrapper-123custom">
       <Carousel>
@@ -29,18 +31,30 @@ function RandomDrinkCarousel() {
           <Carousel.Item key={index}>
             <img src={drink.image_url} alt={drink.name} />
             <Carousel.Caption>
-              <p>{drink.description}</p>
-              {drinks.length > 0 && (
-                <NavLink
-                  className="btn btn-secondary"
-                  to={`/drinks/${drink.id}`}
-                >
-                  {drink.name}
-                </NavLink>
-              )}
-              {drinks.length == 0 && (
-                <button className="btn btn-secondary">{drink.name}</button>
-              )}
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-4 mx-auto">
+                    <div
+                      className="text-white p-3 rounded shadow"
+                      style={{ backgroundColor: "rgba(0, 0, 0, 0.8)" }}
+                    >
+                      <p>{drink.description}</p>
+                      {shuffledDrinks.length > 0 ? (
+                        <NavLink
+                          className="btn btn-secondary"
+                          to={`/drinks/${drink.id}`}
+                        >
+                          {drink.name}
+                        </NavLink>
+                      ) : (
+                        <button className="btn btn-secondary">
+                          {drink.name}
+                        </button>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </Carousel.Caption>
           </Carousel.Item>
         ))}
