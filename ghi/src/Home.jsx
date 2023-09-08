bf2e07c59c1ffa2b55bf3e3a4a687077359bdb1b
Bryan Fischer <myemfar@gmail.com>
2023-09-08 11:49:15 -0600

    added carousel to front page

--- ghi/src/Home.jsx
+++ ghi/src/Home.jsx
@@ -1,9 +1,14 @@
+import RandomDrinkCarousel from "./RandomDrinkCarousel";
+
 const MainPage = () => {
   return (
     <div className="px-4 py-5 my-5 text-center">
       <h1 className="display-5 fw-bold">Barometer</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">WE HELP U DRANK</p>
+      </div>
+      <div>
+        <RandomDrinkCarousel />
       </div>
     </div>
   );
---
+++ ghi/src/RandomDrinkCarousel.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import { useGetDrinkQuery } from "./app/apiSlice";
+import { NavLink } from "react-router-dom";
+
+function RandomDrinkCarousel() {
+  const { data: drinks, isLoading: drinksLoading } = useGetDrinkQuery();
+
+  if (drinksLoading) return <div>Loading..</div>;
+
+  // Function to shuffle an array randomly
+  function shuffleArray(array) {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap elements
+    }
+    return shuffled;
+  }
+
+  // Shuffle the drinks array randomly
+  const shuffledDrinks = shuffleArray(drinks);
+
+  // Select the first 5 drinks from the shuffled array
+  const random5Drinks = shuffledDrinks.slice(0, 5);
+
+  return (
+    <Carousel>
+      {random5Drinks.map((drink, index) => (
+        <Carousel.Item key={index}>
+          <img src={drink.image_url} alt={drink.name} />
+          <Carousel.Caption>
+            <p>{drink.description}</p>
+            <NavLink className="btn btn-secondary" to={`/drinks/${drink.id}`}>
+              {drink.name}
+            </NavLink>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+}
+
+export default RandomDrinkCarousel;
