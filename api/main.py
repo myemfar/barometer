13fe5a2dded3ceae8165b28711e27f65dc1c1198
Bryan Fischer <myemfar@gmail.com>
2023-09-19 16:52:12 -0600

    uncommented dockerfiles

--- api/main.py
+++ api/main.py
@@ -14,9 +14,14 @@ import os
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    os.environ.get("CORS_HOST", None),
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
